095e2f15554c304c8f10d0c16ec20a0a40d7c510
Костя <kostia.freelancer@gmail.com>
2021-05-19 23:08:46 +0300

    update

--- src/compontents/ShopItem/ShopItem.js
+++ src/compontents/ShopItem/ShopItem.js
@@ -6,12 +6,24 @@ const ShopItem = (props) => {
   return (
     <div className="col-sm-6 col-md-4 col-xl-3">
       <div className="item">
+        <div className="item-controls">
+          <div className="edit">
+            <Link className="item-control item-edit" to="/">
+              <i className="fas fa-pen" />
+            </Link>
+          </div>
+          <div className="delete">
+            <button className="item-control item-delete">
+              <i className="fas fa-times" />
+            </button>
+          </div>
+        </div>
         <Link to={"/catalog/" + props.id}>
           <img src={props.src} className="item-img" alt={props.text} />
         </Link>
         <div className="item-body">
           <p>{props.text}</p>
-          <p>{props.price} uah</p>
+          <p>{props.price} UAH</p>
         </div>
       </div>
     </div>
