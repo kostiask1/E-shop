bfd687d9d772c13f5aeb272ba6979af939c1a37d
Костя <kostia.freelancer@gmail.com>
2021-08-25 00:33:06 +0300

    listener fix

--- src/context/Auth/auth-state.js
+++ src/context/Auth/auth-state.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 import { authReducer } from "./auth-reducer";
 import { authContext } from "./auth-context";
@@ -10,7 +10,13 @@ export const AuthState = ({ children }) => {
     const initialState = {
         admin: false,
     };
-    window.addEventListener("storage", () => auth());
+    useEffect(() => {
+        window.addEventListener("storage", auth);
+        return () => {
+            window.removeEventListener("storage", auth);
+        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     const [state, dispatch] = useReducer(authReducer, initialState);
     const encryptWithCryptoJS = (plainText) => {
         const key = CryptoJS.enc.Utf8.parse("hf8685nfhfhjs9h8");
